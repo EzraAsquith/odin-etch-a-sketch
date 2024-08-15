1774e824942d37f53602e9242b9c60166a8d5237
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-14 20:27:22 -0700

    Added button that allows for changing of the grid size

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const container =  document.querySelector(".container");
 
-function getGridSize() {
-    let gridSize = prompt("Enter a size you would like the grid to be")
-    if (gridSize > 100) {
-        gridSize = prompt("Please enter a size smaller than 100, I don't want to kill your computer")
-    }
-    else {
-        return gridSize
-    }
-
-}
-
 
 let gridSize = 16
 const btn = document.querySelector("#btn");
@@ -21,21 +10,27 @@ btn.addEventListener("click", () => {
     }
     else {
         gridSize = gridSize
+        drawGrid(gridSize)
     }
 })
 
-
-for(let i = 0; i < gridSize; i++) {
-    const rowDiv = document.createElement("div");
-    rowDiv.classList.add("row");
-    container.appendChild(rowDiv)
-    for(let j = 0; j < gridSize; j++){
-        const collumnDiv = document.createElement("div")
-        collumnDiv.classList.add("collumn")
-        collumnDiv.addEventListener("mouseenter", () => {
-            collumnDiv.style.backgroundColor = "black"
-        })
-        rowDiv.appendChild(collumnDiv)
+function drawGrid(gridSize){
+    while(container.firstChild) {
+        container.removeChild(container.lastChild)
+    }
+    for(let i = 0; i < gridSize; i++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+        container.appendChild(rowDiv)
+        for(let j = 0; j < gridSize; j++){
+            const collumnDiv = document.createElement("div")
+            collumnDiv.classList.add("collumn")
+            collumnDiv.addEventListener("mouseenter", () => {
+                collumnDiv.style.backgroundColor = "black"
+            })
+            rowDiv.appendChild(collumnDiv)
+        }
     }
 }
 
+drawGrid(gridSize)
