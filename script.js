57dd4fecf9d6f96a0b77d9f161340c31015cfc44
Ezra Asquith <Ezra.asquith@gmail.com>
2024-08-14 20:16:06 -0700

    Grid now fits the page rather than being a fixed size

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const container =  document.querySelector(".container");
 
-for(let i = 0; i < 16; i++) {
+function getGridSize() {
+    let gridSize = prompt("Enter a size you would like the grid to be")
+    if (gridSize > 100) {
+        gridSize = prompt("Please enter a size smaller than 100, I don't want to kill your computer")
+    }
+    else {
+        return gridSize
+    }
+
+}
+
+
+let gridSize = 16
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    gridSize = prompt("Enter a size you would like the grid to be")
+    if (gridSize > 100) {
+        gridSize = prompt("Please enter a size smaller than 100, I don't want to kill your computer")
+    }
+    else {
+        gridSize = gridSize
+    }
+})
+
+
+for(let i = 0; i < gridSize; i++) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
     container.appendChild(rowDiv)
-    for(let j = 0; j < 16; j++){
+    for(let j = 0; j < gridSize; j++){
         const collumnDiv = document.createElement("div")
         collumnDiv.classList.add("collumn")
         collumnDiv.addEventListener("mouseenter", () => {
